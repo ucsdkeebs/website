753ceea3191bd4dfac79191def12a0404534a182
Angela Hu <angelahu925@gmail.com>
2024-03-12 18:24:13 -0700

    Added featured sections

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import About from "../sections/About";
 import Events from "../sections/Events";
+import Featured from "../sections/Featured";
 import Image from 'next/image';
 import raccoon from '../../public/assets/racoon.svg';
 import styles from "../styles/Home.module.css";
@@ -8,16 +9,21 @@ export default function Home() {
   return (
     <>
       <main className={styles.main}>
-      <div className={styles.header_banner}>
-        <h2 className={styles.header}>who we are</h2>
-        <Image className={styles.raccoon_image} src={raccoon} width={100} height={100} alt="raccoon"></Image>
-      </div>
+        <div className={styles.header_banner}>
+          <h2 className={styles.header}>who we are</h2>
+          <Image className={styles.raccoon_image} src={raccoon} width={100} height={100} alt="raccoon"/>
+        </div>
         <About/>
         <div className={styles.header_banner}>
-        <h2 className={styles.header}>upcoming events</h2>
-        <Image className={styles.raccoon_image} src={raccoon} width={100} height={100} alt="raccoon"/>
-      </div>
+          <h2 className={styles.header}>upcoming events</h2>
+          <Image className={styles.raccoon_image} src={raccoon} width={100} height={100} alt="raccoon"/>
+        </div>
         <Events/>
+        <div className={styles.header_banner}>
+          <h2 className={styles.header}>featured club activities</h2>
+          <Image className={styles.raccoon_image} src={raccoon} width={100} height={100} alt="raccoon"/>
+        </div>
+        <Featured/>
       </main>
     </>
   );
---
+++ src/sections/Featured/index.tsx
@@ -0,0 +1,40 @@
+import FeaturedCard from "@/components/FeaturedCard";
+import styles from "./style.module.css";
+
+const featuredInfo = [
+  {
+    id: "01",
+    image: "/assets/keyboard.png",
+    title: "build your own keeb, from scratch",
+    description:
+      "Each quarter, we offer a hands-on opportunity for UCSD students to build their own keyboard or parts of a keyboard. This includes wiring the PCB, making the plate, etc",
+  },
+  {
+    id: "02",
+    image: "/assets/merch.png",
+    title: "keeb club exclusive merch",
+    description:
+      "Support both our club AND our talented artists by buying our keyboard club @ ucsd exclusive merch! Items are group-buy only and takes about 3 months to ship. Limited in-stock quantities available.",
+  },
+  {
+    id: "03",
+    image: "/assets/membership.png",
+    title: "membership card",
+    description:
+      "For a one-time fee get a membership card that will get you discounts to a variety of local San Diego vendors! Offer open to UCSD students only.",
+  },
+];
+
+const Featured: React.FC = () => {
+  return (
+    <div className={styles.featured}>
+      <div className={styles.cards}>
+        {featuredInfo.map((activity) => (
+          <FeaturedCard key={activity.id} activity={activity} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
