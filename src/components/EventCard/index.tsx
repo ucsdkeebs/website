c7cccc014455a07ded7d709c03c6fb300efe8257
cwang04 <117037369+cwang04@users.noreply.github.com>
2024-08-05 15:04:33 -0400

    Changed Time Zone calculation

--- src/components/EventCard/index.tsx
+++ src/components/EventCard/index.tsx
@@ -1,6 +1,12 @@
 import styles from "./style.module.css";
 import Image from "next/image";
 import eventImage from "../../../public/assets/event.png";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc"; //standardizes the time so no client server conflicts
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 export type EventObject = {
   id: string;
@@ -12,10 +18,13 @@ export type EventObject = {
 };
 
 const EventCard: React.FC<{ event: EventObject }> = ({ event }) => {
-  const startDate = new Date(event.startTime);
-  const endDate = new Date(event.endTime);
+  //const startDate = new Date(event.startTime);
+  //const endDate = new Date(event.endTime);
 
-  const formattedStartTime = startDate.toLocaleString("en-US", {
+  const startDate = dayjs.utc(event.startTime).tz('America/Los_Angeles');
+  const endDate = dayjs.utc(event.endTime).tz('America/Los_Angeles');
+
+  /*const formattedStartTime = startDate.toLocaleString("en-US", {
     month: "short",
     day: "numeric",
     year: "numeric",
@@ -28,7 +37,10 @@ const EventCard: React.FC<{ event: EventObject }> = ({ event }) => {
     hour: "numeric",
     minute: "numeric",
     hour12: true,
-  });
+  });*/
+
+  const formattedStartTime = startDate.format("MMM D, YYYY, h:mm A");
+  const formattedEndTime = endDate.format("h:mm A");
 
   const formattedDateTimeRange = `${formattedStartTime} - ${formattedEndTime}`;
   return (
