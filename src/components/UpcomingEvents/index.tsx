5e1326f93f20f951c882b3471104ef0fef4a4eb9
Eric Huang <erhuang623@gmail.com>
2025-02-13 00:50:40 -0800

    New event page
    Tried implementing the Events portion of event page but arrows are not arrowing. The carousel is a pain to get working and I can't seem to align my buttons

--- src/components/UpcomingEvents/index.tsx
+++ src/components/UpcomingEvents/index.tsx
@@ -1,33 +1,50 @@
 import styles from "./style.module.css";
 import Image from "next/image";
-import eventImage from "../../../public/assets/event.png";
-import * as React from 'react';
+import { ArrowIcon } from "./ArrowIcon";
+import event from "@/assets/event.png";
+import ImageCarousel from "../ImageCarousel";
 
-
-const events = [
-  {
-    image: "Path",
-    name: "Event Name",
-    quarter: "Quarter-year"
-  },
-  {
-    image: "Path2",
-    name: "Event Name",
-    quarter: "Quarter-year"
-  },
-  {
-    image: "Path3",
-    name: "Event Name",
-    quarter: "Quarter-year"
-  }];
+const eventImages = [
+  "/public/assets/event.png",
+  "/public/assets/event.png",
+  "/public/assets/event.png",
+];
   
   const UpcomingEvents: React.FC = () => {
     return (
-      <section className="events">
-        <div>
-          <h1>Events</h1>
+      <div className={styles.event_page}>
+        <div className={styles.events_section}>
+          <div className={styles.event_text}>
+          <h1 className={styles.events}>Events</h1>
+          <p className={styles.events_description}>
+            Lorem ipsum dolor sit amet. Et animi consequatur et cumque itaque vel iste neque quo magnam sequi! 
+            A iure voluptatem qui rerum itaque ea officiis inventore et aliquam sunt rem impedit itaque a molestiae 
+            officia? Aut distinctio voluptas rem quod nulla et nisi magni et officiis architecto non modi praesentium nam dicta 
+            voluptatum sit odit quam. Sit vitae dolor eum voluptates repellendus qui quia voluptas ex explicabo eaque qui quia 
+            culpa et galisum saepe. Et sunt harum hic voluptatem expedita qui aliquam laboriosam sit pariat
+          </p>
+          </div>
+        <section className={styles.image_section}>
+          <div className={styles.buttons}>
+            <button className={styles.arrow_left}>
+            <ArrowIcon/> 
+            </button>
+            
+            <img src="/assets/event.png" alt="Keebs Images" className={styles.images}/>
+            <img src="/assets/event.png" alt="Keebs Images" className={styles.images}/>
+            <img src="/assets/event.png" alt="Keebs Images" className={styles.images}/>
+            
+
+            <button className={styles.arrow_right}>
+              <ArrowIcon/>  
+            </button>
+
+          </div>
+        </section>
         </div>
-      </section>
+      </div>
+    
+      
     );
   };
   
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import About from "../sections/About";
 import Featured from "../sections/Featured";
 import Sponsors from "../sections/Sponsors";
 import styles from "../styles/Home.module.css";
+import TitleHeader from "@/components/TitleHeader";
+import Events from "@/sections/Events";
 
 export default function Home() {
   return (
