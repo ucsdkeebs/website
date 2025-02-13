63ca01895f3b113c61a7b484cfa9f9b263cbd44b
Eric Huang <erhuang623@gmail.com>
2025-02-12 19:18:11 -0800

    Description fixes

--- src/components/UpcomingEvents/index.tsx
+++ src/components/UpcomingEvents/index.tsx
@@ -5,19 +5,18 @@ import * as React from 'react';
 
 
 const events = [
-    { title: 'Winter GBM', date: '03 Feb 2023', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
-    { title: 'Love Is In The Keebs', date: '03 Feb 2023', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod...' },
-    { title: 'Studio Keebli', date: '03 Feb 2023', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod...' }
+    { event_name: 'Winter GBM', date: '03 Feb 2023', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' },
+    
   ];
   
   const UpcomingEvents = () => {
     return (
       <div className={styles.event_page}>
-        <h2 className={styles.events}>Upcoming Events</h2>
-
+        <h1 className={styles.eventHeader}>Events</h1>
+        <></>
         {events.map((event, index) => (
           <div className={styles.event_card} key={index}>
-            <div className={styles.event_image}>
+            <div c1lassName={styles.event_image}>
             <Image
             src={eventImage}
             alt="Event Placeholder"
@@ -32,6 +31,7 @@ const events = [
             </div>
           </div>
         ))}
+
     <h2 className={styles.events}>Past Events</h2>
 
     {events.map((event, index) => (
