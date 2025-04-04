2886daf7fb85fce6c056d4f53416e7fe23a31dab
cwang04 <cjwang2004@gmail.com>
2025-04-03 19:54:20 -0700

    fix again

--- src/lib/api/EventAPI.ts
+++ src/lib/api/EventAPI.ts
@@ -4,8 +4,7 @@ import { RsvpResponse } from "../types/apiResponses";
 import axios from "axios";
 
 export const getEvents = async (): Promise<EventObject[]> => {
-  const baseUrl = process.env.NEXT_PUBLIC_KEEBS_API_URL || "https://ucsdkeebs.com/api";
-  const requestUrl = `${baseUrl}${config.api.endpoints.event.getEvents}`;
+  const requestUrl = `${config.api.baseUrl}${config.api.endpoints.event.getEvents}`;
   const response = await axios.get<EventObject[]>(requestUrl);
   return response.data;
 };
